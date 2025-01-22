4ef9561343351fda298e906ca0cdfc117842ab29
Mukesh2212 <mukesh@hola9.com>
2025-01-22 12:00:26 +0000

    frist commit

--- sailoon/settings.py
+++ sailoon/settings.py
@@ -29,6 +29,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'rest_framework',
+    'django_extensions',
     'sailoonapp',
     'rest_framework_simplejwt',
     'rest_framework_simplejwt.token_blacklist',
