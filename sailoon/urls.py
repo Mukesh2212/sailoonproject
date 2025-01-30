7ae7f3a62ad5eaf97d5c179d9fc2185706a734de
Mukesh2212 <mukesh@hola9.com>
2025-01-30 12:30:02 +0000

    commit

--- sailoon/urls.py
+++ sailoon/urls.py
@@ -53,6 +53,8 @@ urlpatterns = [
     path('swagger<format>/', schema_view.without_ui(cache_timeout=0), name='schema-json'),
     path('swagger/', schema_view.with_ui('swagger', cache_timeout=0), name='schema-swagger-ui'),
     path('redoc/', schema_view.with_ui('redoc', cache_timeout=0), name='schema-redoc'),
+    #path('redoc/', schema_view.with_ui('redoc', cache_timeout=0), name='schema-redoc'),
+  
   
 ]
 urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
